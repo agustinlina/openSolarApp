febaa1ad6c93edcf57d8612fba314da961830bbc
Agustín Linares <agustinlinares2009@hotmail.com>
2024-09-22 18:14:21 -0300

    infastructure

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-// index.js
 import express from 'express'
-// import routes from './routes/routes.js'
 import path from 'path'
 import { fileURLToPath } from 'url'
-// import cors from 'cors' // permite el cross origin
+
 const app = express()
 const port = process.env.PORT || 8080
-
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-// app.use(cors())
-// app.use('/', routes)
-
 app.use(express.json()) //
-app.use(express.static(__dirname + '/'))
-// app.use(function (req, res, next) {
-//   res
-//     .status(404)
-//     .send('Sorry cant find that!<br><a href="/">Go back to home</a>')
-// })
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
